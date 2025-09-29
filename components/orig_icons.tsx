4ced1a255ee843f430780c724216b7651a97f2b0
Patrick Ham <patrick.f.ham@gmail.com>
2025-09-29 16:34:51 -0400

    fixed Favicon and Logo

--- components/orig_icons.tsx
+++ components/orig_icons.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { IconSvgProps } from "@/types";
 
 export const Logo: React.FC<IconSvgProps> = ({
-  size = 45,
+  size = 200,
   width,
   height,
   ...props
@@ -11,12 +11,19 @@ export const Logo: React.FC<IconSvgProps> = ({
   <svg
     fill="none"
     height={size || height}
-    viewBox="0 0 45 45"
+    viewBox="0 0 32 32"
     width={size || width}
     {...props}
   >
 
-    <image href="PFHlogo.svg" width="45" height="45" />
+    <path
+      clipRule="evenodd"
+      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
+
+    <image href="../images/PFHlogo.svg" x="0" y="0" width="100" height="100" />
 
   </svg>
 );
